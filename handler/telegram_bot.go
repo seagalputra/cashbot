7fcb9c3f1ada81a0d8745cc6be6d7f96ba7ce998
seagalputra <seagalputra@gmail.com>
2022-10-02 16:32:36 +0700

    feat: refactor and clean-up some code

---
+++ handler/telegram_bot.go
@@ -0,0 +1,197 @@
+package handler
+
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/seagalputra/cashbot/command_history"
+	"github.com/seagalputra/cashbot/expense"
+	"gopkg.in/telebot.v3"
+	"gopkg.in/telebot.v3/middleware"
+)
+
+type TelegramBot struct {
+	CommandHistoryRepo command_history.CommandHistoryRepo
+	ExpenseRepo        expense.ExpenseRepo
+}
+
+var (
+	selector = &telebot.ReplyMarkup{}
+	btnYes   = selector.Data("Yes", "yes")
+	btnNo    = selector.Data("No", "no")
+
+	expenseTypeSelector = &telebot.ReplyMarkup{}
+	btnIncome           = expenseTypeSelector.Data("Income", "income", "income")
+	btnOutcome          = expenseTypeSelector.Data("Outcome", "outcome", "outcome")
+
+	userState = map[string]interface{}{}
+
+	telegramApiToken string
+
+	//go:embed assets/help.txt
+	helpMsg string
+)
+
+func (t *TelegramBot) HandleCommandStart(c telebot.Context) error {
+	username := c.Chat().Username
+	err := t.CommandHistoryRepo.DeleteByUsername(username)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return c.Send("Welcome to Cash Bot")
+}
+
+func (t *TelegramBot) HandleText(c telebot.Context) error {
+	// For handling add expense
+	username := c.Message().Chat.Username
+	history, err := t.CommandHistoryRepo.FindByUsername(username)
+	if err != nil {
+		log.Println(err)
+	}
+
+	step := history.Step
+	userKey := username + "_" + history.CommandName
+	var msg string
+	switch step {
+	case 1:
+		msg = "How many amount do you want to record?"
+		exp := userState[userKey].(*expense.Expense)
+		exp.Title = c.Text()
+		userState[userKey] = exp
+		t.CommandHistoryRepo.IncrementStepByUsername(username)
+	case 2:
+		msg = "When you do the expense?"
+		exp := userState[userKey].(*expense.Expense)
+		amount, err := strconv.Atoi(c.Text())
+		if err != nil {
+			log.Println(err)
+		}
+		exp.Amount = amount
+		userState[userKey] = exp
+		t.CommandHistoryRepo.IncrementStepByUsername(username)
+	case 3:
+		msg = "Here's your expense. Please re-check if it wrongs"
+		exp := userState[userKey].(*expense.Expense)
+		// TODO: change based on user input
+		exp.ExpenseDate = time.Now()
+		userState[userKey] = exp
+		t.CommandHistoryRepo.IncrementStepByUsername(username)
+
+		selector.Inline(
+			selector.Row(btnYes, btnNo),
+		)
+	}
+
+	return c.Send(msg, selector)
+}
+
+func (t *TelegramBot) HandleAddExpense(c telebot.Context) error {
+	msg := "What type of your expense is?"
+	username := c.Message().Chat.Username
+
+	history := command_history.CommandHistory{}
+	history.Username = username
+	history.CommandName = "addexpense"
+
+	t.CommandHistoryRepo.InsertHistory(history)
+
+	userKey := username + "_" + "addexpense"
+	userState[userKey] = &expense.Expense{
+		Username: username,
+	}
+
+	expenseTypeSelector.Inline(
+		expenseTypeSelector.Row(btnIncome, btnOutcome),
+	)
+
+	return c.Send(msg, expenseTypeSelector)
+}
+
+func (t *TelegramBot) HandleHelp(c telebot.Context) error {
+	return c.Send(helpMsg)
+}
+
+func (t *TelegramBot) HandleCancel(c telebot.Context) error {
+	return fmt.Errorf("Not yet implemented!")
+}
+
+func (t *TelegramBot) HandleBtnYes(c telebot.Context) error {
+	username := c.Message().Chat.Username
+	history, err := t.CommandHistoryRepo.FindByUsername(username)
+	if err != nil {
+		log.Println(err)
+	}
+
+	userKey := username + "_" + history.CommandName
+	exp := userState[userKey].(*expense.Expense)
+	err = t.ExpenseRepo.Insert(*exp)
+	if err != nil {
+		log.Println(err)
+	}
+
+	t.CommandHistoryRepo.DeleteByUsername(username)
+	return c.Send("Your expense was successfully added!")
+}
+
+func (t *TelegramBot) HandleBtnNo(c telebot.Context) error {
+	return c.Respond()
+}
+
+func (t *TelegramBot) HandleBtnIncome(c telebot.Context) error {
+	username := c.Message().Chat.Username
+	history, err := t.CommandHistoryRepo.FindByUsername(username)
+	if err != nil {
+		log.Println(err)
+	}
+
+	userKey := username + "_" + history.CommandName
+	exp := userState[userKey].(*expense.Expense)
+	exp.Type = c.Data()
+	userState[userKey] = exp
+
+	t.CommandHistoryRepo.IncrementStepByUsername(username)
+
+	msg := "Please provide the title of the expense"
+
+	return c.Send(msg)
+}
+
+func (t *TelegramBot) HandleBtnOutcome(c telebot.Context) error {
+	return c.Respond()
+}
+
+func (t *TelegramBot) SetupTelegramBot() {
+	telegramApiToken = os.Getenv("TELEGRAM_BOT_API_KEY")
+
+	pref := telebot.Settings{
+		Token: telegramApiToken,
+		Poller: &telebot.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	}
+
+	b, err := telebot.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Use(middleware.Logger())
+
+	b.Handle("/addexpense", t.HandleAddExpense)
+	b.Handle("/cancel", t.HandleCancel)
+	b.Handle("/help", t.HandleHelp)
+	b.Handle("/start", t.HandleCommandStart)
+	b.Handle(&btnIncome, t.HandleBtnIncome)
+	b.Handle(&btnNo, t.HandleBtnNo)
+	b.Handle(&btnOutcome, t.HandleBtnOutcome)
+	b.Handle(&btnYes, t.HandleBtnYes)
+	b.Handle(telebot.OnText, t.HandleText)
+
+	b.Start()
+}
--- main.go
+++ main.go
@@ -2,48 +2,26 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"os"
 	"path/filepath"
-	"strconv"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/seagalputra/cashbot/command_history"
 	"github.com/seagalputra/cashbot/expense"
-	"gopkg.in/telebot.v3"
-	"gopkg.in/telebot.v3/middleware"
+	"github.com/seagalputra/cashbot/handler"
 	_ "modernc.org/sqlite"
 )
 
 var (
 	db *sql.DB
-
-	selector = &telebot.ReplyMarkup{}
-	btnYes   = selector.Data("Yes", "yes")
-	btnNo    = selector.Data("No", "no")
-
-	expenseTypeSelector = &telebot.ReplyMarkup{}
-	btnIncome           = expenseTypeSelector.Data("Income", "income", "income")
-	btnOutcome          = expenseTypeSelector.Data("Outcome", "outcome", "outcome")
-
-	userState = map[string]interface{}{}
-
-	telegramApiToken string
-
-	helpMsg = `
-I can help you to record your money expenses.
-
-You can control me by sending the following commands:
-
-/addexpense - add new expense
-/editexpense - edit an expense
-/viewexpense - view an available expense
-/configure - config your Google Sheets account
-/help - show available commands
-`
 )
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	// setup sqlite db
@@ -54,159 +32,15 @@ func main() {
 	}
 	defer db.Close()
 
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	telegramApiToken = os.Getenv("TELEGRAM_BOT_API_KEY")
+	LoadEnv()
 
 	commandHistoryRepo := &command_history.CommandHistoryRepo{DB: db}
 	expenseRepo := &expense.ExpenseRepo{DB: db}
 
-	pref := telebot.Settings{
-		Token: telegramApiToken,
-		Poller: &telebot.LongPoller{
-			Timeout: 10 * time.Second,
-		},
+	telegramBot := &handler.TelegramBot{
+		CommandHistoryRepo: *commandHistoryRepo,
+		ExpenseRepo:        *expenseRepo,
 	}
 
-	b, err := telebot.NewBot(pref)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	b.Use(middleware.Logger())
-
-	b.Handle(telebot.OnText, func(c telebot.Context) error {
-		// For handling add expense
-		username := c.Message().Chat.Username
-		history, err := commandHistoryRepo.FindByUsername(username)
-		if err != nil {
-			log.Println(err)
-		}
-
-		step := history.Step
-		userKey := username + "_" + history.CommandName
-		var msg string
-		switch step {
-		case 1:
-			msg = "How many amount do you want to record?"
-			exp := userState[userKey].(*expense.Expense)
-			exp.Title = c.Text()
-			userState[userKey] = exp
-			commandHistoryRepo.IncrementStepByUsername(username)
-		case 2:
-			msg = "When you do the expense?"
-			exp := userState[userKey].(*expense.Expense)
-			amount, err := strconv.Atoi(c.Text())
-			if err != nil {
-				log.Println(err)
-			}
-			exp.Amount = amount
-			userState[userKey] = exp
-			commandHistoryRepo.IncrementStepByUsername(username)
-		case 3:
-			msg = "Here's your expense. Please re-check if it wrongs"
-			exp := userState[userKey].(*expense.Expense)
-			// TODO: change based on user input
-			exp.ExpenseDate = time.Now()
-			userState[userKey] = exp
-			commandHistoryRepo.IncrementStepByUsername(username)
-
-			selector.Inline(
-				selector.Row(btnYes, btnNo),
-			)
-		}
-
-		return c.Send(msg, selector)
-	})
-
-	b.Handle("/start", func(c telebot.Context) error {
-		username := c.Chat().Username
-		err := commandHistoryRepo.DeleteByUsername(username)
-		if err != nil {
-			log.Println(err)
-		}
-
-		return c.Send("Welcome to Cash Bot")
-	})
-
-	b.Handle("/addexpense", func(c telebot.Context) error {
-		msg := "What type of your expense is?"
-		username := c.Message().Chat.Username
-
-		history := command_history.CommandHistory{}
-		history.Username = username
-		history.CommandName = "addexpense"
-
-		commandHistoryRepo.InsertHistory(history)
-
-		userKey := username + "_" + "addexpense"
-		userState[userKey] = &expense.Expense{
-			Username: username,
-		}
-
-		expenseTypeSelector.Inline(
-			expenseTypeSelector.Row(btnIncome, btnOutcome),
-		)
-
-		return c.Send(msg, expenseTypeSelector)
-	})
-
-	b.Handle("/help", func(c telebot.Context) error {
-		return c.Send(helpMsg)
-	})
-
-	b.Handle("/cancel", func(c telebot.Context) error {
-		return fmt.Errorf("Not yet implemented!")
-	})
-
-	b.Handle(&btnYes, func(c telebot.Context) error {
-		username := c.Message().Chat.Username
-		history, err := commandHistoryRepo.FindByUsername(username)
-		if err != nil {
-			log.Println(err)
-		}
-
-		userKey := username + "_" + history.CommandName
-		exp := userState[userKey].(*expense.Expense)
-		err = expenseRepo.Insert(*exp)
-		if err != nil {
-			log.Println(err)
-		}
-
-		commandHistoryRepo.DeleteByUsername(username)
-		return c.Send("Your expense was successfully added!")
-	})
-
-	b.Handle(&btnNo, func(c telebot.Context) error {
-		return c.Respond()
-	})
-
-	b.Handle(&btnIncome, func(c telebot.Context) error {
-		username := c.Message().Chat.Username
-		history, err := commandHistoryRepo.FindByUsername(username)
-		if err != nil {
-			log.Println(err)
-		}
-
-		userKey := username + "_" + history.CommandName
-		exp := userState[userKey].(*expense.Expense)
-		exp.Type = c.Data()
-		userState[userKey] = exp
-
-		commandHistoryRepo.IncrementStepByUsername(username)
-
-		msg := "Please provide the title of the expense"
-
-		return c.Send(msg)
-	})
-
-	b.Handle(&btnOutcome, func(c telebot.Context) error {
-		return c.Respond()
-	})
-
-	b.Start()
+	telegramBot.SetupTelegramBot()
 }
